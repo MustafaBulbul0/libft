5ab4e554028497f24428a578bdc9764f80e4e9b6
Mustafa Bulbul <mustafa@example.com>
2024-10-12 00:12:34 +0300

    update

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -3,13 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mubulbul <mubulbul@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mustafa <mustafa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 14:36:09 by mustafa           #+#    #+#             */
-/*   Updated: 2024/10/10 19:39:58 by mubulbul         ###   ########.fr       */
+/*   Updated: 2024/10/11 23:41:06 by mustafa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void *ft_memcpy(void *dest, const void *src, size_t n) {
+    unsigned char*  nDest;
+    unsigned char*  nSrc;
+    size_t          i;
 
+    i = 0;
+    nDest = (unsigned char *)dest;
+    nSrc = (unsigned char *)src;
+    while (i < n)
+    {
+        nDest[i] = nSrc[i];
+        i++;
+    }
+    return (dest);
+}
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -3,13 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mubulbul <mubulbul@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mustafa <mustafa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:05:42 by mustafa           #+#    #+#             */
-/*   Updated: 2024/10/10 19:39:56 by mubulbul         ###   ########.fr       */
+/*   Updated: 2024/10/11 23:47:38 by mustafa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void *ft_memmove(void *dest, const void *src, size_t n) {
+    unsigned char*  nDest;
+    unsigned char*  nSrc;
+    size_t          i;
 
+    i = 0;
+    nDest = (unsigned char *)dest;
+    nSrc = (unsigned char *)src;
+    if (nDest < nSrc) {
+        while (i < n) {
+            nDest[i] = nSrc[i];
+            i++;
+        }
+    } else {
+        while (i < sizeof(src)) {
+            nDest[i] = nSrc[i];
+            i++;
+        }
+    }
+    return (dest);
+}
