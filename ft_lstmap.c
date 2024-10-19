b986dbe693659da2e13efb0deb1d171c21567511
Mustafa Bulbul <mubulbul@k1m07s07.42istanbul.com.tr>
2024-10-19 23:39:50 +0300

    update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mubulbul <mubulbul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:05:38 by mubulbul          #+#    #+#             */
-/*   Updated: 2024/10/19 11:51:36 by mubulbul         ###   ########.fr       */
+/*   Updated: 2024/10/19 23:36:59 by mubulbul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	(void)lst;
-	(void)f;
-	(void)del;
+	t_list	*null;
+	t_list	*new_list;
 
-	return (NULL);
+	if (!lst || !f || !del)
+		return (NULL);
+	null = NULL;
+	while (lst)
+	{
+		new_list = ft_lstnew(f(lst->content));
+		if (!new_list)
+		{
+			ft_lstclear(&null, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&null, new_list);
+		lst = lst->next;
+	}
+	return (null);
 }
